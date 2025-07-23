d6395311e79cadf27802fece7d4fa912eb46503a
enveramboy <157831942+enveramboy@users.noreply.github.com>
2025-07-23 13:32:40 -0400

    Reworked evaluation class to instead be a function. Added precision, recall, f1_score calculations.

--- evaluation.py
+++ evaluation.py
@@ -1,10 +1,31 @@
-class Evaluator:
-    def __init__(self):
-        self.TP = 0
-        self.TN = 0
-        self.FP = 0
-        self. FN = 0
 
-    def Evaluate_Model(self, model_results):
-        # TODO: update TP, TN, FP, FN based off model results
-        pass
+def Evaluate(model_results):
+    """
+    Calculates Precision, Recall, and F1-Score of the model's results.
+
+    Args:
+        model_results: Data structure containing features, labels, and predictions.
+    
+    Returns:
+        results: Dictionary containing Precision, Recall, and F1 Score of the model's predictions.
+    """
+
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+    # TODO: update TP, TN, FP, FN based off model results
+
+    # Calculate Precision, Recall, and F1-Score
+    precision = TP/(TP + FP)
+    recall = TP/(TP + FN)
+    f1_score = 2 * (precision * recall)/(precision + recall)
+
+    # Store results in results dictionary
+    results = {
+        "Precision": precision,
+        "Recall": recall,
+        "F1 Score": f1_score
+    }
+
+    return results
