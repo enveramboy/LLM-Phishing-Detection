943816b8ab742d3ad76debfb8b371375aea7d698
enveramboy <157831942+enveramboy@users.noreply.github.com>
2025-07-23 13:02:29 -0400

    Basic formatting for an Evaluation class

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,10 @@
-print("hooray!")
+class Evaluator:
+    def __init__(self):
+        self.TP = 0
+        self.TN = 0
+        self.FP = 0
+        self. FN = 0
+
+    def Evaluate_Model(self, model_results):
+        # TODO: update TP, TN, FP, FN based off model results
+        pass
