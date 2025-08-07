c6ba3e8d9411e3359f88bcc7b48577cff6fecab0
enveramboy <157831942+enveramboy@users.noreply.github.com>
2025-08-07 13:20:49 -0400

    Ran algorithm.py on 300 sample subset, also added documentation to subset.py

--- subset.py
+++ subset.py
@@ -8,24 +8,24 @@ class Subset_Maker():
     Object that encapsulates subset creation of a given csv
     """
     def __init__(self, csv):
+        """
+        Initializes a new Subset_Maker instance.
+
+        Args:
+            csv (str): Path to CSV file to make a subset of.
+        """
         self.base_df = pd.read_csv(csv)
         self.base_legit = self.base_df[self.base_df['Class'] == 'Legit']
         self.base_phishing = self.base_df[self.base_df['Class'] == 'Phishing']
         self.subset = pd.DataFrame()
-    # def Generate_Subset(self, size):
-    #     legit_pool = self.base_legit.copy()
-    #     phishing_pool = self.base_phishing.copy()
-    #     for i in range(int(int(size)/2)):
-    #         sample_idx = random.randint(0, len(legit_pool)-1)
-    #         print(f"Sample Index: {sample_idx}")
-    #         self.subset._append(legit_pool.iloc[sample_idx], ignore_index=True)
-    #         legit_pool = legit_pool.drop(sample_idx, axis='index')
-            
-    #         sample_idx = random.randint(0, len(phishing_pool)-1)
-    #         self.subset._append(phishing_pool.iloc[sample_idx], ignore_index=True)
-    #         phishing_pool = phishing_pool.drop(sample_idx, axis='index')
-    #     self.subset.to_csv(f"cleaned_phishing_subset_{size}.csv", index=False)   
+
     def Generate_Subset(self, size):
+        """
+        Generates a new random subset of a specified size from the stored CSV file.
+
+        Args:
+            size (int): Number of rows in the new subset.
+        """
         data = []
         data.append(self.base_legit.sample(n=int(int(size)/2), random_state=1))
         data.append(self.base_phishing.sample(n=int(int(size)/2), random_state=1))
@@ -40,6 +40,10 @@ def main() -> None:
 
     subset_maker = Subset_Maker(pathlib.Path(args.input))
     subset_maker.Generate_Subset(args.size)
+
+# Example usage:
+# read from cleaned_phishing_dataset.csv and derive a subset of size 300
+# python subset.py --input cleaned_phishing_dataset.csv --size 300
 
 if __name__ == "__main__":
     main()
